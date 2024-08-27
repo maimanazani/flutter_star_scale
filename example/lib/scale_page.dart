10a54eb3f4f783ea251ffa74c6c7bcff00ffe39b
Aiman Azani <m.aimanazani@gmail.com>
2024-08-27 14:49:17 +0800

    Update scale_page.dart

--- example/lib/scale_page.dart
+++ example/lib/scale_page.dart
@@ -89,13 +89,12 @@ class _ScalePageState extends State<ScalePage> {
                 data.status == ScaleStatus.disconnect_failed)
               TextButton(
                 onPressed: () {
-                  // if (data.status == ScaleStatus.connect_success) {
-                  //   starScale.disconnect();
-                  // } else if (data.status == ScaleStatus.disconnect_success ||
-                  //     data.status == ScaleStatus.connect_failed) {
-                  //   starScale.connect(widget.connectionInfo);
-                  // }
-                  starScale.disconnect();
+                  if (data.status == ScaleStatus.connect_success) {
+                    starScale.disconnect();
+                  } else if (data.status == ScaleStatus.disconnect_success ||
+                      data.status == ScaleStatus.connect_failed) {
+                    starScale.connect(widget.connectionInfo);
+                  }
                 },
                 style: ButtonStyle(
                   shape: WidgetStateProperty.all<RoundedRectangleBorder>(
